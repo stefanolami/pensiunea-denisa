823f4ebbce3c7889b4a8b56197de4b36520a5020
stefanolami <stefanolami90@gmail.com>
2024-04-16 16:44:41 +0300

    done mobileMenu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -2,6 +2,7 @@
 
 import { motion, MotionConfig } from 'framer-motion'
 import { useState } from 'react'
+import Link from 'next/link'
 
 export default function MobileMenu() {
 	const [active, setActive] = useState(false)
@@ -13,8 +14,9 @@ export default function MobileMenu() {
 			}}
 		>
 			<motion.button
+				initial={false}
 				onClick={() => setActive((pv) => !pv)}
-				className="relative h-16 w-16 rounded-full"
+				className="relative h-16 w-16 rounded-full z-50"
 				animate={active ? 'open' : 'closed'}
 			>
 				<motion.span
@@ -64,17 +66,40 @@ export default function MobileMenu() {
 					variants={{
 						open: {
 							rotate: ['0deg', '0deg', '45deg'],
-							bottom: ['35%', '50%', '50%'],
 							left: '50%',
+							bottom: ['35%', '50%', '50%'],
 						},
 						closed: {
 							rotate: ['45deg', '0deg', '0deg'],
-							bottom: ['50%', '50%', '35%'],
 							left: 'calc(50% + 8px)',
+							bottom: ['50%', '50%', '35%'],
 						},
 					}}
 				/>
 			</motion.button>
+			<motion.div>
+				<motion.nav
+					className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-blur flex flex-col gap-4 justify-center items-center"
+					initial={false}
+					animate={active ? 'open' : 'closed'}
+					variants={{
+						open: {
+							opacity: 1,
+							pointerEvents: 'auto',
+						},
+						closed: {
+							opacity: 0,
+							pointerEvents: 'none',
+						},
+					}}
+				>
+					<Link href="/about">About Us</Link>
+					<Link href="/prices">Prices</Link>
+					<Link href="/deals">Deals</Link>
+					<Link href="/gallery">Gallery</Link>
+					<Link href="/contact">Contact</Link>
+				</motion.nav>
+			</motion.div>
 		</MotionConfig>
 	)
 }
